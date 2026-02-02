495d23d11fa04cc5eca0a6da91c15f22968c4781
Dream <thomas.czarnecki.dev@gmail.com>
2026-02-03 05:58:50 +0900

    Task/2 - Install Primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
+    provideAnimationsAsync(),
+    // provideAnimations()
   ]
 };
