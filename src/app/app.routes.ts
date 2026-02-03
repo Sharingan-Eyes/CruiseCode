e47d87199562163e23e1900bf3156ae53eb2b6a5
Dream <thomas.czarnecki.dev@gmail.com>
2026-02-03 23:27:18 +0900

    #3 - Set app structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./features/home/home').then(
+                (c) => c.Home
+            ),
+        // canActivate: [authedGuardCanActivate],
+    },
+    {
+        path: 'forbidden',
+        loadComponent: () =>
+            import('./features/forbidden/forbidden').then(
+                (c) => c.Forbidden
+            ),
+    },
+    {
+        path: '',
+        redirectTo: 'operator',
+        pathMatch: 'full',
+    },
+    { path: '**', redirectTo: '' },
+];
